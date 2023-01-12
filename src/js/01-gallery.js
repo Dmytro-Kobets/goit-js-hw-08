8d7b7497dcadba72a46ff2c955acabc03ef9e5b5
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-01-12 20:33:02 +0200

    mod-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function galleryMarkup(items) {
+  return items
+    .map(
+      item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>`
+    )
+    .join('');
+}
+gallery.innerHTML = galleryMarkup(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash';
 
+const player = new Player(document.querySelector('iframe'));
+
+function playtimeSave () {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+    })
+}
+
+player.on('timeupdate', throttle(playtimeSave, 1000));
