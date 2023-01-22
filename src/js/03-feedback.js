c6b9a840ef027618003de50fb195e7eb628d9078
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-01-22 18:53:35 +0200

    mod-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,17 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector("feedback-form");
+const form = document.querySelector('.feedback-form');
+const fbEmail = document.querySelector('#email');
+const fbMessage = document.querySelector('#message');
 
-function DataSave () {
-    DataSave.preventDefault();
-    
+function DataSave(e) {
+  e.preventDefault();
+  const feedback = {
+    email: fbEmail.value,
+    message: fbMessage.value,
+  };
+  const data = JSON.stringify(feedback);
+  localStorage.setItem('feedback-form-state', data);
 }
+
+form.addEventListener('input', throttle(DataSave, 500));
