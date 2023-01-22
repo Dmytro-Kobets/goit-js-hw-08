8351a7fb65a93804a830aa62b557a8787ef98bc4
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-01-22 22:04:32 +0200

    mod-4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,14 +4,29 @@ const form = document.querySelector('.feedback-form');
 const fbEmail = document.querySelector('#email');
 const fbMessage = document.querySelector('#message');
 
-function DataSave(e) {
-  e.preventDefault();
-  const feedback = {
-    email: fbEmail.value,
-    message: fbMessage.value,
-  };
-  const data = JSON.stringify(feedback);
-  localStorage.setItem('feedback-form-state', data);
+let data = {};
+
+function dataSave(e) {
+  data[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
-form.addEventListener('input', throttle(DataSave, 500));
+function dataSubmit(e) {
+  e.preventDefault();
+  form.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('input', throttle(dataSave, 500));
+form.addEventListener('submit', dataSubmit);
+
+function preload() {
+  const preData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (preData) {
+    fbEmail.value = preData.email || '';
+    fbMessage.value = preData.message || '';
+  }
+}
+
+preload();
